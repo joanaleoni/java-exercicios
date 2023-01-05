5c9eb2db0816d757b2c0711bcf444cb9a2d9b355
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-05 08:17:24 -0300

    18-RemoveProductIf (Lambda expression inline - Predicate)

--- 18-RemoveProductIf/src/domain/Product.java
+++ 18-RemoveProductIf/src/domain/Product.java
@@ -29,14 +29,6 @@ public class Product {
         this.price = price;
     }
 
-    public static boolean staticProductPredicate(Product p) {
-        return p.getPrice() >= 100.0;
-    }
-
-    public boolean nonStaticProductPredicate() {
-        return price >= 100.0;
-    }
-
     @Override
     public String toString() {
         return name + ", " + String.format("%.2f", price);
