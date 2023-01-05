f8e9c24b61b65166a17a5984277652d83c14d4fe
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-05 08:18:09 -0300

    19-UpperCaseName (Lambda expression inline - Function)

---
+++ 19-UpperCaseName/src/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+import domain.Product;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author joana
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+        
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+        
+        List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+        names.forEach(System.out::println);
+    }
+
+}
